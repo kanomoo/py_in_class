9ededb77b9bd5a8136c98705295a9bed3dbb55fb
few717254 <few717254@gmail.com>
2026-05-28 14:17:31 +0700

    refactor: optimize factorial calculation with memoization and update twoSum function structure

--- Hundred_Recursive/Recersive-01.py
+++ Hundred_Recursive/Recersive-01.py
@@ -1,7 +1,13 @@
+# def calculate_factorial(n: int) -> int:
+#     if n < 0: raise ValueError("Factorial is not defined for negative number ")
+#     if n == 0 or n == 1: return 1
+#     return n * calculate_factorial(n - 1)
+
 def calculate_factorial(n: int) -> int:
-    if n < 0: raise ValueError("Factorial is not defined for negative number ")
-    if n == 0 or n == 1: return 1
-    return n * calculate_factorial(n - 1)
+    memo = {}
+    if n in (0, 1): return 1
+    if n not in memo: memo[n] = n * calculate_factorial(n-1)
+    
 
 if __name__ == "__main__":
-    print(calculate_factorial(-3))
+    print(calculate_factorial(10))
--- Leet_Code/1.Two_sum.py
+++ Leet_Code/1.Two_sum.py
@@ -6,9 +6,8 @@
 
 class Solution:
     def twoSum(self, nums: list[int], target: int) -> list[int]:
-        for i in range(len(nums)):
-            for j in range(len(nums)):
-                if nums[i] + nums[j] == target and i != j: return [i, j]
+        dict = {}
+        # if
 
 
 if __name__ == "__main__":
